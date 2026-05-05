a0544624be9a4fda79365a42f0cb6bef0d756eb4
MARCO-LL-07 <llantoy2007@gmail.com>
2026-05-05 09:22:51 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/edit/:id" element={<EditPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
